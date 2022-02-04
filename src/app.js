9718e1e30a57f869f867a8c59832d95e033bd67a
sarang <sarang>
2022-02-04 22:29:55 +0530

    ********** JavaScript closures and the useEffect hook*********** 1) stopwatch.js  with time depds , [time] 2)stopwatch.js  with empty array depds [] , where closure issue exist , becaue our useEffect executiong  only once 3) stopwatch.js  with on/off buttoon functinality

--- src/app.js
+++ src/app.js
@@ -1,22 +1,11 @@
 import React, { useEffect, useState } from "react";
 import "./machine.css";
-import Counter from "./Counter";
+import StopWatch from "./stopwatch";
 
 const App = () => {
-  const [show, SetShow] = useState(true);
-
-  const handleonCLickHandler = () => {
-    SetShow(!show);
-  };
-
   return (
     <React.Fragment>
-      {show && <Counter />}
-      <br />
-      <br />
-      <button type="button" onClick={handleonCLickHandler}>
-        show/hide
-      </button>
+      <StopWatch />
     </React.Fragment>
   );
 };
---
+++ src/stopwatch.js
@@ -0,0 +1,186 @@
+import { useEffect, useState } from "react";
+
+const StopWatch = () => {
+  const [time, setTime] = useState(0);
+  const [counterActive, setCounterActive] = useState(true);
+
+  const onCLickHandler = () => {
+    // setCounterActive(!counterActive);
+    setCounterActive((oldValue) => !oldValue);
+  };
+
+  useEffect(() => {
+    console.log("----effect function-----");
+    let interval = null;
+    if (counterActive) {
+      interval = setInterval(() => {
+        setTime((oldvalue) => oldvalue + 1);
+      }, 1000);
+    }
+
+    return () => {
+      console.log("----cleanup function-----");
+      clearInterval(interval);
+    };
+  }, [counterActive]);
+
+  return (
+    <section>
+      <h1>Stop Counter</h1>
+      <div>
+        <span>{time}</span>
+      </div>
+      <button onClick={onCLickHandler}>start/stop</button>
+    </section>
+  );
+};
+
+export default StopWatch;
+
+/*
+
+stopwatch.js functionality with time depds , [time]
+--------------------------------------------------
+import React, { useEffect, useState } from "react";
+
+const StopWatch = () => {
+  console.log("================================");
+  const [time, setTime] = useState(0);
+  const [counterActive, setCounterActive] = useState(true);
+
+  useEffect(() => {
+    console.log("I am a effet function ");
+    let interval = null;
+    interval = setInterval(() => {
+      setTime(time + 1);
+    }, 1000);
+
+    return () => {
+      console.log("I am a effet cleanup function function ");
+      clearInterval(interval);
+    };
+  }, [time]);
+
+  const onCLickHandler = () => {
+    console.log("I am counteractive click handler");
+    setCounterActive(!counterActive);
+  };
+
+  return (
+    <section>
+      <h1>Ultimate Stop Counter</h1>
+      <div>
+        <span>{time}</span>
+      </div>
+
+      <button onClick={onCLickHandler}>start/stop</button>
+    </section>
+  );
+};
+
+export default StopWatch;
+
+
+
+*/
+
+/*
+
+stopwatch.js  with empty array depds [] , where closure issue exist , becaue our useEffect executiong  only once
+-----------------------------------------------------------------------------------------------------------
+try to explain in whiteboard why it is taking only freeze time value and how we can using fix by setState callback function	
+	
+import React, { useEffect, useState } from "react";
+
+const StopWatch = () => {
+  console.log("================================");
+  const [time, setTime] = useState(0);
+  const [counterActive, setCounterActive] = useState(true);
+
+  useEffect(() => {
+    console.log("I am a effet function ");
+    let interval = null;
+    interval = setInterval(() => {
+      //setTime(time + 1);
+      // issue fixed using callback function
+      setTime((time) => time + 1);
+      console.log("work");
+    }, 1000);
+
+    return () => {
+      console.log("I am a effet cleanup function function ");
+      clearInterval(interval);
+    };
+  }, []);
+
+  const onCLickHandler = () => {
+    console.log("I am counteractive click handler");
+    setCounterActive(!counterActive);
+  };
+
+  return (
+    <section>
+      <h1>Ultimate Stop Counter</h1>
+      <div>
+        <span>{time}</span>
+      </div>
+
+      <button onClick={onCLickHandler}>start/stop</button>
+    </section>
+  );
+};
+
+export default StopWatch;
+
+*/
+
+/*
+stopwatch.js  with on/off buttoon functinality
+-----------------------------------------------------------------------------------------------------------	
+	
+import React, { useEffect, useState } from "react";
+
+const StopWatch = () => {
+  console.log("================================");
+  const [time, setTime] = useState(0);
+  const [counterActive, setCounterActive] = useState(true);
+
+  useEffect(() => {
+    console.log("I am a effet function ");
+    let interval = null;
+    if (counterActive) {
+      interval = setInterval(() => {
+        //setTime(time + 1);
+        // issue fixed using callback function
+        setTime((time) => time + 1);
+        console.log("work");
+      }, 1000);
+    }
+
+    return () => {
+      console.log("I am a effet cleanup function function ");
+      clearInterval(interval);
+    };
+  }, [counterActive]);
+
+  const onCLickHandler = () => {
+    console.log("I am counteractive click handler");
+    //setCounterActive(!counterActive);
+    setCounterActive((counterActive) => !counterActive);
+  };
+
+  return (
+    <section>
+      <h1>Ultimate Stop Counter</h1>
+      <div>
+        <span>{time}</span>
+      </div>
+
+      <button onClick={onCLickHandler}>start/stop</button>
+    </section>
+  );
+};
+
+export default StopWatch;
+
+*/
